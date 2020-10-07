49b28ff44aa54b69946bec073e15430a458f9e85
Raymond Law <raymondlaw@users.noreply.github.com>
2020-10-07 05:24:17 -0400

    Update BirthdayEmitter.js

--- 04/modules/BirthdayEmitter.js
+++ 04/modules/BirthdayEmitter.js
@@ -7,7 +7,7 @@ class BirthdayEmitter extends EventEmitter {
             let month = Number.parseInt(`${mm_dd[0]}${mm_dd[1]}`);
             let day   = Number.parseInt(`${mm_dd[3]}${mm_dd[4]}`);
             birthdays
-                .filter(birthday => birthday.month === month && birthday.day === day)
+                .filter(birthday => birthday.month == month && birthday.day == day)
                 .forEach(birthday => this.emit("birthday", {birthday}));
         });
     }
